7158f43d87074fbfbc489a68870867967dc2ae67
Z1mm3r <czimmer@ramapo.edu>
2019-05-11 22:07:45 -0400

    more changes

--- src/PageComponents/Home.js
+++ src/PageComponents/Home.js
@@ -1,43 +1,48 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader.js'
-import {Grid, Container, Segment} from 'semantic-ui-react'
-
+import {Grid, Container, Segment, Header,Image} from 'semantic-ui-react'
+import picture from '../Media/Images/horizon.jpg';
 export default function HomeScreen(){
+
+
 
 
   return(
     <div>
       <PageHeader/>
-      <div className="home-body">
-        <div className = "home-video">
-          <video className="video-body" autoplay="true" loop id="vid" muted>
-            <source src="../Media/Images/road.mp4" type="video/mp4"/>
-            <source src="https://i.giphy.com/media/jlraqCNnuvyfD0jUmr/source.mp4" type="video/mp4"/>
-          </video>
-          <div className='home-content'>
-             <Grid columns={3} divided centered style={{height: '100%',width: '100%'}}>
-               <Grid.Column width={1}>
-                  hello
-               </Grid.Column>
-               <Grid.Column width={10}>
-                 <div className='home-content-main'>
-                   <Container>
-                     <Segment>
-                       The Brown Cow Jumped Over The Moon
-                     </Segment>
-                     <Segment>
-                        Surely you Could aswell?
-                     </Segment>
-                   </Container>
-                 </div>
-               </Grid.Column>
-               <Grid.Column width={1}>
-                 test
-               </Grid.Column>
-             </Grid>
-           </div>
-        </div>
-      </div>
+      <Grid divided centered >
+        <Grid.Row height={3}>
+          <Header> Dare To Be Different</Header>
+            <div className="ui fluid image">
+              <Image centered src={picture}/>
+              <div style={{color: 'white', position: 'absolute', bottom: 0, width: '100%', height: 'auto'}}>TEST</div>
+            </div>
+        </Grid.Row>
+        <Grid.Row height={10}>
+          <div>
+            <Grid columns={3} divided centered style={{height: '100%',width: '100%'}}>
+              <Grid.Column width={1}>
+                left
+              </Grid.Column>
+              <Grid.Column width={10}>
+                <div className='home-content-main'>
+                  <Container>
+                    <Segment>
+                      The Brown Cow Jumped Over The Moon
+                    </Segment>
+                    <Segment>
+                      Surely you Could aswell?
+                    </Segment>
+                  </Container>
+                </div>
+              </Grid.Column>
+              <Grid.Column width={1}>
+                right
+              </Grid.Column>
+            </Grid>
+          </div>
+        </Grid.Row>
+      </Grid>
     </div>
   )
 
